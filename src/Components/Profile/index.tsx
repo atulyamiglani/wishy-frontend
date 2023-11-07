df146180856b9e97f15eb2b984a3f92365bd97b5
Hunter Groff <huntergroff22@gmail.com>
2023-11-06 22:37:37 -0500

    add tailwind and react router

---
+++ src/Components/Profile/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+  return (
+    <div>
+      <img src="https://via.placeholder.com/150" alt="Profile Picture" />
+      <h1>Name</h1>
+    </div>
+  );
+};
+
+export default Profile;
