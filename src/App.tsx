fee8026e78a1c336afe6d5bc63f6d9f4c256c8ea
Atulya Miglani <atulyamiglani02@gmail.com>
2023-11-08 14:18:52 -0500

    context and sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Profile from "./Components/Profile";
 import Navbar from "./Components/navbar";
 import Search from "./Components/Search";
+import SignUp from "./Components/SignUp";
+
+type User = {
+  name: string;
+  email: string;
+};
+
+export const CurrentUserContext = createContext<{
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}>({ user: null, setUser: () => {} });
 
 function App() {
+  const [user, setUser] = useState<null | User>(null);
+
   return (
-    <BrowserRouter>
-      {/* Main Navigation */}
-      <Navbar />
+    <CurrentUserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        {/* Main Navigation */}
+        <Navbar />
 
-      {/* This is to make sure the navbar doesn't overlap the content */}
-      <div className="h-16"></div>
+        {/* This is to make sure the navbar doesn't overlap the content */}
+        <div className="h-16"></div>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </CurrentUserContext.Provider>
   );
 }
 
---
+++ src/Components/SignUp/index.tsx
@@ -0,0 +1,90 @@
+import { useContext } from "react";
+import { CurrentUserContext } from "../../App";
+
+const SignUp: React.FC<{}> = () => {
+  const { user, setUser } = useContext(CurrentUserContext);
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"></div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Create an Account
+        </h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        {/* TODO: this form thing was adding query params to the url. 
+        commented it out for now. Password shouldnt be in the query params lol. */}
+        {/* <form className="space-y-6"> */}
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Name
+          </label>
+          <div className="mt-2">
+            <input
+              id="name"
+              name="name"
+              type="name"
+              autoComplete="email"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Create Password
+            </label>
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            className="flex w-full justify-center rounded-md bg-purple-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => {
+              alert("gimme account");
+            }}
+          >
+            Sign up
+          </button>
+        </div>
+        {/* </form> */}
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
