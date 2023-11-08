ac1671f50631784f7629aad765ad2fed8d0b2473
Atulya Miglani <atulyamiglani02@gmail.com>
2023-11-08 15:15:20 -0500

    add to nav bar

--- src/Components/navbar.tsx
+++ src/Components/navbar.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
-    return (
-        <nav className="bg-white fixed top-0 w-full shadow-md p-4 flex items-center justify-between">
-            <Link to="/" className="text-3xl font-bold text-purple-600 hover:text-blue-600">Wishy</Link>
-            <div className="flex space-x-4">
-                <Link to="/profile" className="text-lg text-purple-600 hover:text-blue-600">Profile</Link>
-                <Link to="/search" className="text-lg text-purple-600 hover:text-blue-600">Search</Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="bg-white fixed top-0 w-full shadow-md p-4 flex items-center justify-between">
+      <Link
+        to="/"
+        className="text-3xl font-bold text-purple-600 hover:text-blue-600"
+      >
+        Wishy
+      </Link>
+      <div className="flex space-x-4">
+        <Link
+          to="/sign-up"
+          className="text-lg text-purple-600 hover:text-blue-600"
+        >
+          Sign Up
+        </Link>
+        <Link
+          to="/login"
+          className="text-lg text-purple-600 hover:text-blue-600"
+        >
+          Login
+        </Link>
+        <Link
+          to="/profile"
+          className="text-lg text-purple-600 hover:text-blue-600"
+        >
+          Profile
+        </Link>
+        <Link
+          to="/search"
+          className="text-lg text-purple-600 hover:text-blue-600"
+        >
+          Search
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
