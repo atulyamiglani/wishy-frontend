125c37a2156714c8f1333b69bf24df718901d331
Atulya Miglani <atulyamiglani02@gmail.com>
2023-12-02 16:27:53 -0500

    search

---
+++ src/Components/ProductDetails/index.tsx
@@ -0,0 +1,11 @@
+import { ProductInfo } from "../../App";
+
+interface ProductsDetailsProps {
+  product: ProductInfo;
+}
+
+const ProductsDetails: React.FC<ProductsDetailsProps> = ({ product }) => {
+  return <></>;
+};
+
+export default ProductsDetails;
